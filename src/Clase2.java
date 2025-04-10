f6219c815afe211c1176c14b68a1e4c2d54fea2d
dam <dam@203P18.mshome.net>
2025-04-10 11:23:24 +0200

    Segundo commit

---
+++ src/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public void mostrarRama(){
+        System.out.println("Estas en la rama PRODUCCION");
+    }
+    public void mostrarNombreApellidos(){
+        System.out.println("Marcos Lopez Ortego");
+    }
+}
