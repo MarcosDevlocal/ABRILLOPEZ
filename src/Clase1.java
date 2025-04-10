2a1f176b45202edd9f62c7ed5ef955f6f8e7b355
dam <dam@203P18.mshome.net>
2025-04-10 11:14:14 +0200

    Primer commit

---
+++ src/Clase1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+   public void mostrarNombre(){
+       System.out.println("Marcos Lopez Ortego");
+   }
+}
